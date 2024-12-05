9e1698d5ecb54bbad9fb3106f18ba39529d29c34
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2024-12-05 20:03:24 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+'use strict';
+let total = 0;
+let average = 0;
+const numbers = [
+    ...document.querySelectorAll('span.population')
+];
+const population = numbers.map((item)=>item.textContent.replaceAll(',', ''));
+for (const number of population)total += Number(number);
+average = total / population.length;
+document.querySelector('span.total-population').textContent = total.toLocaleString('en-US');
+document.querySelector('span.average-population').textContent = Math.floor(average).toLocaleString('en-US');
+
+//# sourceMappingURL=index.f75de5e1.js.map
